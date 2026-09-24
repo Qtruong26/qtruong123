bdb94dc554d443e7de22fbfbad88b080f93dc509
Qtruong26 <dieptruong666@gmail.com>
2026-09-24 20:18:23 +0700

    sua loi dang nhap

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,9 +25,6 @@ const app = express();
 app.use(cors({
   origin: [
     'https://fitcore-gym-app.vercel.app',
-    'http://localhost:3000',
-    'http://localhost:5500',
-    'http://127.0.0.1:5500'
   ],
   credentials: true
 }));
